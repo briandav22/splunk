c3a19d4d63f2c2a5f6ce88b819325ecd043d4c86
unknown <briandav22@gmail.com>
2020-09-28 09:11:21 -0400

    reformating

--- wpost_splunk.py
+++ wpost_splunk.py
@@ -8,13 +8,25 @@ from datetime import datetime, timedelta
 # time range we want to use for the search (last 24 hours by default)
 number_of_hours = 24
 
+# server information 
+host = '10.1.4.58'
+port = '8089'
+username = 'admin'
+password = 'somepass'
+
+# splunk query 
+
+searchquery_oneshot = "search 10.1.5.1"
+
+
+
 # creates the data in a way the Splunk API expects it. 
 earliest_time = str((datetime.utcnow() - timedelta(hours = number_of_hours)).strftime('%Y-%m-%dT%H:%M:%S'))
 
 
 # creates connection to Splunk 
 
-service = client.connect(host='10.1.4.58',port='8089', username='briandav',  password='Belathedog')
+service = client.connect(host=host,port=port, username=username,  password=password)
 
 
 # time frame arguments 
@@ -23,9 +35,6 @@ kwargs_oneshot = {"earliest_time": earliest_time,
                   "latest_time": 'now',}
 
 
-# search we are going to use 
-
-searchquery_oneshot = "search 10.1.5.1"
 
 oneshotsearch_results = service.jobs.oneshot(searchquery_oneshot, **kwargs_oneshot)
 
